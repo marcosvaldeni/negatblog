feee6885675e7a21d30d166f7359d664f87f5f57
Marcos Valdeni Lucas <marcosvaldeni@gmail.com>
2023-02-15 21:36:29 +0000

    main header aligning

--- src/pages/Home/HomeHeader/index.tsx
+++ src/pages/Home/HomeHeader/index.tsx
@@ -1,22 +1,31 @@
-import img from '../../../assets/image.jpg'
+import img from "../../../assets/image.jpg";
 
 export function HomeHeader() {
   return (
-    <header className="-mt-20 mx-3 p-8 w-full bg-base-profile rounded-xl shadow-lg flex justify-between items-center gap-8">
+    <header className="-mt-20 mx-3 p-8 w-full h-fit bg-base-profile rounded-xl shadow-lg flex justify-between gap-8">
       <img src={img} alt="" className="w-36 h-36 rounded-lg" />
 
-      <div>
-        <div className="flex">
-          <h1>Cameron Williamson</h1>
-          <a href="#">GITHUB</a>
-        </div>
-        <p>
+      <div className="flex flex-col justify-between">
+        <div>
+          <div className="font-bold flex justify-between items-start">
+            <h1 className="text-2xl">Cameron Williamson</h1>
+            <a className="text-xs text-blue mt-1" href="#">GITHUB</a>
+          </div>
+        <p className="mt-2">
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu 
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat 
+          pulvinar vel mass.
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu 
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat 
+          pulvinar vel mass.
           Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu 
           viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat 
           pulvinar vel mass.
         </p>
+
+        </div>
         <div>
-          <ul className="flex">
+          <ul className="flex gap-2 mt-4">
             <li>cameronwll</li>
             <li>Rocketseat</li>
             <li>seguidores</li>
